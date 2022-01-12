b63554c9256deae7cd159d6a7089a536f21ae204
hemanthpulimi <97596174+hemanthpulimi@users.noreply.github.com>
2022-01-12 15:46:34 +0530

    Add files via upload

---
+++ ARID/ARID/Scientist.swift
@@ -0,0 +1,18 @@
+//
+//  Scientist.swift
+//  ARID
+//
+//  Created by Hemanth Pulimi on 04/05/19.
+//  Copyright © 2019 AP2. All rights reserved.
+//
+
+import Foundation
+
+struct Scientist: Decodable {
+    let name: String
+    let dates: String
+    let field: String
+    let bio: String
+    let country: String
+    let source: String
+}
